96e163c9e2aded833b30624c2e47f0198fd36e51
Akshay Bhatia <akshaybhatia10@gmail.com>
2017-08-26 15:24:18 +0530

    test script

---
+++ test.py
@@ -0,0 +1,16 @@
+from LibraryML import *
+
+"""
+For Testing Purposes only
+Builds and runs a graph with LibraryML.
+"""
+x, y = Input(), Input()
+
+f = Add(x, y)
+
+feed_dict = {x: 10, y: 5}
+
+sorted_nodes = topological_sort(feed_dict)
+output = forward_pass(f, sorted_nodes)
+
+print("{} + {} = {} (according to miniflow)".format(feed_dict[x], feed_dict[y], output))
