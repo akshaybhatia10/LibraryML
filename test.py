c7f4010c5c80df1b7b4c84730024d2d2d85d7a94
Akshay Bhatia <akshaybhatia10@gmail.com>
2017-08-26 16:10:29 +0530

    minor change

--- test.py
+++ test.py
@@ -7,10 +7,16 @@ Builds and runs a differential graph with LibraryML.
 x, y, z = Input(), Input(), Input()
 
 f = Add(x, y, z)
+s = Mul(x, y, z)
 
 feed_dict = {x: 10, y: 5, z: 1}
 
 sorted_nodes = topological_sort(feed_dict)
-output = forward_pass(f, sorted_nodes)
 
-print("{} + {} + {} = {} (according to miniflow)".format(feed_dict[x], feed_dict[y], feed_dict[z], output))
+add = forward_pass(f, sorted_nodes)
+mul = forward_pass(s, sorted_nodes)
+
+
+print("{} + {} + {} = {} ".format(feed_dict[x], feed_dict[y], feed_dict[z], add))
+
+print("{} * {} * {} = {} ".format(feed_dict[x], feed_dict[y], feed_dict[z], mul))
