ceb192378dd71208c71bbff638f9edbba99e8b69
Akshay Bhatia <akshaybhatia10@gmail.com>
2017-08-28 16:18:47 +0530

    minor update cost function

--- test.py
+++ test.py
@@ -7,16 +7,22 @@ Builds and runs a differential graph with LibraryML.
 x, y, z = Input(), Input(), Input()
 #inputs, weights, bias = Input(), Input(), Input()
 X, W, b = Input(), Input(), Input()
+y, y_hat = Input(), Input()
 
 X_ = np.array([[-1., -2.], [-1, -2]])
 W_ = np.array([[2., -3], [2., -3]])
 b_ = np.array([-3., -5])
+
+y_ = np.array([1, 2, 3])
+y_hat_ = np.array([4.5, 5, 10])
+
 
 f = Add(x, y, z)
 s = Mul(x, y, z)
 #l = Linear(inputs, weights, bias)
 l = Linear(X, W, b)
 g = Sigmoid(l)
+c = cost_mse(y, y_hat)
 
 
 feed_dict1 = {x: 10, y: 5, z: 1}
@@ -29,16 +35,19 @@ feed_dict1 = {x: 10, y: 5, z: 1}
 
 feed_dict2 = {X: X_, W: W_, b: b_}
 
+feed_dict3 = {y: y_, y_hat: y_hat_}
+
 
 
 sorted_nodes1 = topological_sort(feed_dict1)
 sorted_nodes2 = topological_sort(feed_dict2)
-
+sorted_nodes3 = topological_sort(feed_dict3)
 
 add = forward_pass(f, sorted_nodes1)
 mul = forward_pass(s, sorted_nodes1)
 lin = forward_pass(l, sorted_nodes2)
 sig = forward_pass(g, sorted_nodes2)
+cost = forward_pass(c, sorted_nodes3)
 
 
 print("{} + {} + {} = {} ".format(feed_dict1[x], feed_dict1[y], feed_dict1[z], add))
@@ -48,5 +57,7 @@ print("{} * {} * {} = {} ".format(feed_dict1[x], feed_dict1[y], feed_dict1[z], m
 print("Linear Output: {}".format(lin))
 
 print("Sigmoid of Linear Operation: {}".format(sig))
+
+print("Cost: {}".format(cost))
 
 
